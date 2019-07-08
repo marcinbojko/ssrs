7bf16779a998a9d54e87aa2c47eb763b5ba6f5fd
Angelos Bousias <angelos@bousias.net>
2019-07-08 12:54:18 +0200

    Update checksum for 14.0.600.1274 release

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName        = 'ssrs'
 $scriptPath         = $(Split-Path $MyInvocation.MyCommand.Path)
 $url64              = "https://download.microsoft.com/download/E/6/4/E6477A2A-9B58-40F7-8AD6-62BB8491EA78/SQLServerReportingServices.exe"
-$checksum64         = "99aaffce8a003839e69e4fad8bcbbc3a71d946902f876df421210dbb7aa6a3c7"
+$checksum64         = "bd0c482a4b0f381388ce512e7e38cd5e466b9e8945ebc632d0d3b0b2a8a2aa3a"
 $logfile            = "$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log"
 $logdir             = "$env:TEMP\chocolatey\$($packageName)"
 $killexec           = 0
